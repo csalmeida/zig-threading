f6f417d7678df94888c0a4bdf853606906a47016
Cristiano <cs.almeida@protonmail.ch>
2025-08-02 14:24:06 -0500

    Adds notes to routines.

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,6 @@ pub fn main() !void {
   const cpu_count = try std.Thread.getCpuCount();
   std.debug.print("System has {} CPU cores\n", .{cpu_count});
 
-  single.runRoutine();
-  try multi.runRoutine();
+  single.runRoutine(); // Single threaded code.
+  try multi.runRoutine(); // Multi threaded code.
 }
